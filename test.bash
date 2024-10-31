e79c9d1abb963d65bfbcfae5274ee843d7b3ca89
Keiyo Nakayama <keiyou0206@gmail.com>
2024-10-31 12:46:57 +0900

    Add a command

---
+++ test.bash
@@ -0,0 +1,23 @@
+#!/bin/bash -xv
+
+ng (){
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+++ seq 5
+++ ./plus
++ out=15
+[ "${res}" = 15 ] || "$LINENO"
++ '[' 15 = 15 ']'
+
+[ "${res}" = 0 ] && echo OK
++ '[' 0 = 0 ']'
++ echo OK
+OK
+exit $res
++ exit 0
+
