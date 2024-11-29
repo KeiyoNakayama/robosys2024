74ee563e6b6ff98b66002a9f781ef00cdab95d63
Keiyo Nakayama <keiyou0206@gmail.com>
2024-11-29 11:22:06 +0900

    [fix] test

--- test.bash
+++ test.bash
@@ -22,7 +22,7 @@ out=$(echo | ./plus)
 
 for i in $(seq 0 100); do
     out=$(echo "$i" | ./kadai1)
-	if [[ "$out" != "正解！挑戦回数: $attempts" ]]; then
+	if [[ "$out" != "正解！挑戦回数: i" ]]; then
 		res = 0
 		[ "$?" = 0 ] || ng "$LINENO"
 		break
